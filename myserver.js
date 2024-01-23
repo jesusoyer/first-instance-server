ec4f397b929215610deaf302c8d4649f89dd5063
jesusoyer <oyervidesjesus017@gmail.com>
2024-01-23 16:35:51 -0600

    changed code to include nodemailer

--- myserver.js
+++ myserver.js
@@ -4,29 +4,21 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = 5001;
 
-// Enable CORS
 app.use(cors({ origin: "http://3.82.151.217:5001" }));
-
-// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Serve static files (you may need to adjust the path)
 app.use(express.static("public"));
 
-// Logging middleware to log incoming requests
 app.use((req, res, next) => {
   console.log(`Received request: ${req.method} ${req.url}`);
   next();
 });
 
-// Create a transporter using your email service credentials
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
@@ -38,13 +30,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Handle form submissions
 app.post("/submit-form", async (req, res) => {
   console.log("Received form submission:", req.body);
 
   const { name, email, message } = req.body;
 
-  // Define the email content
   const mailOptions = {
     from: email,
     to: process.env.USERNAME,
@@ -53,7 +43,6 @@ app.post("/submit-form", async (req, res) => {
   };
 
   try {
-    // Send the email
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent: " + info.response);
     res.status(200).send("Form submitted successfully");
@@ -63,7 +52,6 @@ app.post("/submit-form", async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
